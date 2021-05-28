69f63aef2658e1bc1bb364102308fb43efba5332
ŞEYHMUS ASKAR <seyhmus.as@gmail.com>
2021-05-28 18:22:34 +0300

    First Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(myMessage),
-      ),g
+      ),
       body: buildBody(context)
     );
   }
